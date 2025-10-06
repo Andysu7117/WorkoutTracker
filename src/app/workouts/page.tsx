d52c2e26e4a082dd9dda4aba9154431e78b1326f
Andy <andy.su7117@gmail.com>
2025-10-06 16:06:09 +1100

    workouts page

---
+++ src/app/workouts/page.tsx
@@ -0,0 +1,7 @@
+import { HydrateClient } from "~/trpc/server";
+
+export default async function Workouts() {
+    return <main className="flex min-h-screen flex-col bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <h1>This is the workouts page</h1>
+    </main>
+}
